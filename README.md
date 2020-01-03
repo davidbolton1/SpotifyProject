04611f02d831f88c1fdcfa40e94a865ba30b1d3d
David Bolton <smdeox@live.com>
2020-01-03 17:52:14 -0500

    Working Version

---
+++ README.md
@@ -0,0 +1,65 @@
+# Concergenics
+
+
+Catie Evans and David Bolton's first major project utilizing API's from Spotify and Eventful.
+The goal of this project was to create a landing page that would allow the user to log-in to their Spotify account, and once the log-in was confirmed by Spotify we would redirect them to a new page that would display their top 10 artists. The user would then have the ability to see future concert data for their artists.
+
+## Code
+
+A backend is created and allows a user to log-in to their Spotify account. Upon validating a user's credentials, Catie wrote the code and set the endoints to redirect a user to our "logged-in" page.
+David then wrote multiple fetch functions, the first returns a list of the logged-in user's top 10 artists with proper formatting. We then map through each artist to create a card that we will use later.
+```javascript
+async function fetchArtistNames() {
+    return await fetch('https://api.spotify.com/v1/me/top/artists', settings)
+    .then((response) => {return response.json()})
+      .then((data) => {
+        let listOfNames = data.items.map((item) => {
+          return item.name.split(' ').join('%20');
+        })
+        return listOfNames;
+      });
+  };
+  ```
+  A second fetch request is then ran, returning an array of URLs that link to an image of each artist.
+  ```javascript
+  async function fetchArtistPictures() {
+    return await fetch('https://api.spotify.com/v1/me/top/artists', settings)
+    .then((response) => {return response.json()})
+      .then((data) => {
+        let listOfImages = data.items.map((item) => {
+          return item.images[1].url
+        })
+        return listOfImages;
+      });
+  };
+```
+If a user tries to log-in with an incorrect password, we return an error message to the user and give them the opportunity to try to log-in again.
+
+
+David wrote various functions to link the user's Spotify data with the Eventful API. Functions were added to take a given Spotify artist and make a fetch request to Eventful, returning the start time, venue name, venue address, and a url to purchase tickets for their next 5 concerts.
+
+## Design
+
+Along with creating a redirect page that stored the user's Spotify authorization token, Catie constructed the framework of both our pages and then devised a styling layout for each.
+
+## Technologies Used
+JavaScript, HTML, CSS, Bootstrap 4, Node, Express
+
+## Stretch Goals
+We would like to add a display all events that would show all upcoming events of all artists. We have the functional code written but haven't diplayed it. We would like to add more filtering features, allowing a user to search by zip code/city. 
+
+## Challenges
+The Spotify API is a challenge to use because of how serious it takes user privacy. Storing a personal auth token and asking for the right permissions to access certain user data was difficult at the beginning. 
+
+
+## Collaborators 
+
+[Catie's GitHub](https://github.com/evanscatie)
+
+[David's Github](https://github.com/Aerohob)
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
---
+++ authorization_code/public/app.js
@@ -0,0 +1,167 @@
+// --------------------------------------------------------------APP-MAIN-JS (INCLUDES SPOTIFY)------------------------------------------------------
+
+// ACCESS TOKEN SCRIPT 
+// __________________________________________________________________________________________________
+
+// < Obtains parameters from the hash of the URL @return Object >
+function getHashParams() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+  while ( e = r.exec(q)) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+}
+
+// < Saves access token as variable >
+var params = getHashParams();
+var access_token = params.access_token,
+    refresh_token = params.refresh_token,
+    error = params.error;
+
+console.log(access_token);
+
+
+
+
+
+
+
+// -------------------------------------------------------------------SPOTIFY----------------------------------------------------------------------------
+
+// FETCH REQUEST TO SPOTIFY API FOR TOP ARTIST
+// ___________________________________________________________________________________________________
+
+// Defines required settings to access Spotify API
+let settings = {
+  "headers": {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${access_token}`,
+  }
+}
+
+// Function creates fetch request to Spotify API and returns the user's top 10 artist and returns (name: med picture) as an object.
+
+window.onload = (fetchArtists());
+
+
+async function fetchArtists() {
+  return await fetch('https://api.spotify.com/v1/me/top/artists', settings)
+  .then((response) => {return response.json()})
+    .then((data) => {
+      let listOfArtists = data.items.slice(0, 10).map((item) => {
+        return {
+         name: item.name, 
+         picture: item.images[0].url
+        }
+      })
+      //console.log(listOfArtists)
+      return listOfArtists;
+         
+
+
+  // CREATES ARTIST CARD
+  // ___________________________________________________________________________________________________
+
+    }).then((listOfArtists) => {
+      listOfArtists.forEach((artist) => {
+          let createCard = document.createElement('div');
+          createCard.class = "card"
+          createCard.innerHTML = `
+          <a href='#header'>
+          <div class="row card-row justify-content-center">
+            <div class="col-md-6">
+              <div class="card text-black text-center ">
+                <img src="${artist.picture}" class="card-img rounded-circle shadow-lg p-0 mb-4  rounded" alt="...">
+                    <div id="${artist.name}" class="card-title font-weight-bold bg-transparent">${artist.name}
+                      <div class="row-md-6">
+                        <button class="upcoming-shows-btn btn btn-outline-light">View Upcoming Shows</button>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+          </a>
+          `
+      createCard.addEventListener('click', () => {
+        let hiddenContainer= document.querySelector(".test-container")
+        hiddenContainer.innerHTML = '';
+        hiddenContainer.style.display = "block";
+         getEventDetails(`${artist.name}`)
+          //console.log(getEventDetails(`${artist.name}`))
+        })
+       let container = document.querySelector(".artist-grid")
+        container.appendChild(createCard);
+      })
+    });
+    
+};
+
+// ADDS EVENT DATA TO ARTIST CARD
+// ___________________________________________________________________________________________________
+// function generateEventCards(artist) {
+//   buttonDiv = document.getElementById(artist)
+//   artist = artist.toLowerCase()
+
+//   console.log(artist)
+//   const makeRequest = async () => {
+//     await getEventDetails(artist).then(function(eventDetails) {
+//       if (eventDetails.events != null){
+//         events = eventDetails.events.event
+//         console.log(events)
+//         for (var i = 0; i < events.length; i++) {
+//           buttonDiv.innerHTML += `
+//             <div class="row card-row justify-content-center">
+//               <div class="col-md-6">
+//                 <div class="card bg-transparent text-black text-center ">
+//                   <div class="card-footer font-weight-bold bg-transparent">
+//                     ${events[i].title}
+//                     <div class="bg-transparent event-location">
+//                       ${events[i].venue_name} ***** ${events[i].start_time.slice(0,10)}
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//             `
+//         }
+//       } else {
+//         buttonDiv.innerHTML += "We're sorry - events are unavailable for this artist at this time"
+//       } 
+//     })
+//   }
+//   makeRequest() 
+
+
+// < ERROR HANDLING FOR USER LOGIN >
+// ___________________________________________________________________________________________________
+if (error) {
+  alert('There was an error during the authentication');
+} else {
+  if (access_token) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        },
+    });
+  } else {
+      // render initial screen
+      $('#login').show();
+      $('#loggedin').hide();
+  }
+      document.getElementById('obtain-new-token').addEventListener('click', function() {
+        $.ajax({
+          url: '/refresh_token',
+          data: {
+            'refresh_token': refresh_token
+          }
+        }).done(function(data) {
+          access_token = data.access_token;
+        });
+      }, false);
+
+  };
